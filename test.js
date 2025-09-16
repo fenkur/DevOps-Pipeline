adba09bbe5da19b6d8dbde15b9ac7edc9425cfc8
Fenky Wah <fenkwah15@gmail.com>
2025-09-16 12:10:43 -0500

    fixed: test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 console.log('Starting tests...');
-setTimeOut(() => console.log('Waiting 3 seconds..'), 3000);
+setTimeout(() => console.log('Waiting 3 seconds...'), 3000);
 console.log('Tests completed!');
